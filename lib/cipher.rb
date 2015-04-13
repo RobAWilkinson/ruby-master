53baa9701a798448f9eb8f8b980879996e17aafa
David Lee <jinetix@gmail.com>
2015-04-13 10:46:24 -0700

    Update cipher.rb

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,3 +1,20 @@
 class Cipher
+	    attr_reader :abc
+
+    def encrypt(string, shift)
+	    @abc = ('a'..'z').to_a.join
+	    i = shift % abc.size
+	    string.downcase!.gsub!(/\s+/, "")
+	    string.tr(@abc, @abc[i..-1] + @abc[0...i])
+    end
+
+    def decrypt(string, shift)
+	    @abc = ('a'..'z').to_a.join
+	    i = shift % abc.size
+	    string.tr(@abc[i..-1] + @abc[0...i], @abc)
+    end
 
 end
+
+code = Cipher.new
+code.encrypt("Test test", -5)
