9c4ece78a64691c81d572317eb47c2e7088980c2
David Lee <jinetix@gmail.com>
2015-04-13 11:04:23 -0700

    Update cipher.rb

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -4,17 +4,12 @@ class Cipher
     def encrypt(string, shift)
 	    @abc = ('a'..'z').to_a.join
 	    i = shift % abc.size
-	    string.downcase!.gsub!(/\s+/, "")
-	    string.tr(@abc, @abc[i..-1] + @abc[0...i])
+	    string = string.downcase
+	    string.delete!(' ')
+	    string.tr!(@abc, @abc[i..-1] + @abc[0...i])
     end
-
-    def decrypt(string, shift)
-	    @abc = ('a'..'z').to_a.join
-	    i = shift % abc.size
-	    string.tr(@abc[i..-1] + @abc[0...i], @abc)
-    end
-
+    
 end
 
 code = Cipher.new
-code.encrypt("Test test", -5)
+code.encrypt("sdffdSDAFd sadsf", 1)
